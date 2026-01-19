352c3e8271cabf5f4dfa79894cc21ea2c233f5f9
A-0234 <eddypeng0234@gmail.com>
2026-01-18 18:23:50 -0600

    Update init.lua

--- mods/ comod_iron/init.lua
+++ mods/ comod_iron/init.lua
@@ -1,6 +1,5 @@
 
 minetest.after(0, function()
-
     -- Recipe: 1 steel block → 9 steel ingots
     minetest.register_craft({
         output = "default:steel_ingot 9",
@@ -8,5 +7,17 @@ minetest.after(0, function()
             {"default:steelblock"}
         }
     })
+    
+	minetest.register_craft({
+		type = "cooking",
+		output = "default:steel_ingot",
+		recipe = "technic:wrought_iron_dust",
+		cooktime = 8
+	})
+
 
 end)
+
+
+
+
